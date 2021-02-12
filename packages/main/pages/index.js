cbbabb7afce8f907f797a275a996e69d04dae7af
mahardicka nurachman <mahardicka404@gmail.com>
2021-02-12 13:08:27 +0700

    setup main core

---
+++ packages/main/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Main App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="#">Main App</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
